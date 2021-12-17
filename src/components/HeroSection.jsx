17fa14f6cb5422b57fcdbb3159058164dda72877
vishal lambe <vishal238128@gmail.com>
2021-12-17 10:55:17 +0530

    initialise

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Button from './Button'
+import { Link } from 'react-router-dom'
+import './HeroSection.css'
+
+
+const HeroSection = ({
+    lightBg, topLine, lightText, lightTextDesc, headline, description, buttonLabel, img, alt, imgStart
+}) => {
+    return (
+        <>
+            <div className={lightBg ? 'home_hero-section' : 'home_hero-section darkBg'}>
+            <div className='container'>
+                <div className="row home__hero-row" style={{display:'flex', flexDirection: imgStart==='start' ? 'row-reverse' : 'row'}}>
+                    <div className="col">
+                        <div className="home_hero-text-wrapper">
+                            <div className="top-line">{topLine}</div>
+                            <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+                            <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>{description}</p>
+                            <Link to='/sign-up'>
+                                <Button buttonSize='btn--wide' buttonColor='blue'>{buttonLabel}</Button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="home__hero-img-wrapper">
+                            <img src={img} alt={alt} className='home__hero-img' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>  
+        </>
+    )
+}
+
+export default HeroSection
---
+++ src/components/pages/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjOne } from './Data';
+
+
+function Products() {
+  return (
+    <>
+      <HeroSection {...homeObjOne} />
+    
+    </>
+  );
+}
+
+export default Products;
